cd7c82c73253a295725f2f197389fad8b09956ef
Gustavo Oliveira <gustavo.oliver456@gmail.com>
2021-12-19 02:16:57 -0300

    first commit

---
+++ Login em PHP/Teste.php
@@ -0,0 +1,10 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>Login e Senha</title>
+</head>
+<body>
+
+    </div>
+</body>
+</html> 
---
+++ Login em PHP/index.php
@@ -0,0 +1,21 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>Login e Senha</title>
+    <link rel="stylesheet" href="CSS/style.css">
+
+</head>
+<body>
+    <div id="corpo-from">
+    <h1 class="title-form">Entrar</h1>
+
+
+        <form method="POST" Saction="dados.php">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Senha">
+            <input type="submit" value="Login">
+            <h4 class="title-form"> Não tem uma conta? <a href="">CRIE UMA AQUI</a>
+        </form>
+    </div>
+</body>
+</html> 
