90a14f36fd5dfcd6c9c7174290470d055ecabcdc
Gustavo Oliveira <gustavo.oliver456@gmail.com>
2021-12-20 18:18:41 -0300

    Registre

--- Login em PHP/register.php
+++ Login em PHP/register.php
@@ -1,1 +1,23 @@
-Feito
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>SISTEMA DE LOGIN</title>
+    <link rel="stylesheet" href="CSS/style.css">
+
+</head>
+<body>  
+
+    <div id="corpo-from-regis">
+    <h4 class="title-form" id="facalogin">Bem-vindo(a) a minha plataforma.</h4>
+    <h2 class="title-form">Cadastre-se</h2>
+        <form method="POST" Saction="data.php">
+            <input type="text" placeholder="Nome">
+            <input type="email" placeholder="E-mail">
+            <input type="password" placeholder="Sua senha">
+            <input type="password" placeholder="Confirme sua senha">
+            <input type="submit" value="CADASTRE">
+            <h5 class="title-form" id="cadastrar"> Já tem uma conta? <a href="index.php">LOGIN</a>
+        </form>
+    </div>
+</body>
+</html> 
