897ee9496652984fa14aadb61f21c461ed76fd66
Gustavo Oliveira <gustavo.oliver456@gmail.com>
2021-12-21 10:46:14 -0300

    Amazon 2.0

--- Login em PHP/register.php
+++ Login em PHP/register.php
@@ -1,7 +1,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="utf-8"/>
-    <title>Projeto login em PHP</title>
+    <title>AMAZON PRIME 2.0</title>
     <link rel="stylesheet" href="CSS/style.css">
     
 
@@ -9,7 +9,7 @@
 <body>  
 
     <div id="corpo-from-regis">
-    <h4 class="title-form" id="facalogin">Bem-vindo(a) a minha plataforma.</h4>
+    <h4 class="title-form" id="facalogin">Bem-vindo(a) a plataforma.</h4>
     <h2 class="title-form">Cadastre-se</h2>
         <form method="POST" Saction="data.php">
             <input type="text" placeholder="Nome">
