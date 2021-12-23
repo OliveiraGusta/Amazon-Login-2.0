5ee27c731eb6e789ac306935f03cd2d527deabd3
OliveiraGusta <gustavo.oliver456@gmail.com>
2021-12-23 19:18:45 -0300

    users.php 1/2

---
+++ Login em PHP/CLASS/users.php
@@ -0,0 +1,52 @@
+<?php
+
+Class User{
+
+    private $pdo;
+    public $msgError = "";
+
+    public function connection($name, $host, $user, $password){
+        global $pdo;
+        global $msgError;
+
+        try{
+            $pdo = new PDO("mysql:dbname=" .$name.";host=".$host,$user,$password);
+        } catch(PDOException $e){
+            $msgError =  $e ->getMessage ();
+        } 
+    }
+
+
+    public function register($name, $email, $cpf,$password){
+        global $pdo; 
+
+        $sql= $pdo ->prepare("SELECT id_user FROM user WHERE email = :e"); 
+        $sql->bindValue(":e", $email);
+        $sql->execute();
+
+            if($sql->rowCount() > 0){
+                return false;
+            }
+            else{
+                $sql= $pdo->prepare("INSERT INTO users(name, email, cpf, password) VALUES (:n, :e, :c, :p)");
+                $sql->bindValue(":n", $name);
+                $sql->bindValue(":e", $email);
+                $sql->bindValue(":c", $cpf);
+                $sql->bindValue(":p", $password);
+                $sql->execute();
+                return true;
+        }
+
+
+    }
+
+    public function login($email,$password){
+        global $pdo;
+
+
+    }
+}
+
+
+
+?>
