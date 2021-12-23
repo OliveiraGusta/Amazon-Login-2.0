294186d67d9e5af791dcc474832165ac0426461c
OliveiraGusta <gustavo.oliver456@gmail.com>
2021-12-23 17:17:00 -0300

    body-div

--- Login em PHP/register.php
+++ Login em PHP/register.php
@@ -8,15 +8,15 @@
 </head>
 <body>  
 
-    <div id="corpo-from-regis">
+    <div id="body-from-regis">
     <h4 class="title-form" id="login">Bem-vindo(a) a plataforma.</h4>
     <h2 class="title-form">Cadastre-se</h2>
         <form method="POST">
             <input type="text" name="name" placeholder="Nome" maxlength="30">
             <input type="email" name="email" placeholder="E-mail" maxlength="40"> 
             <input type="tel" name="cpf"  placeholder="CPF" pattern="[0-9]{11}" maxlength="11">
-            <input type="password" name="password"  placeholder="Sua senha" maxlength="20">
-            <input type="password" name="confPassword"  placeholder="Confirme sua senha" maxlength="20" >
+            <input type="password" name="password"  placeholder="Sua senha" maxlength="30">
+            <input type="password" name="confPassword"  placeholder="Confirme sua senha" maxlength="30" >
             <input type="submit" value="CADASTRE">
             <h5 class="title-form" id="register"> Já tem uma conta? <a href="index.php">FAÇA SEU LOGIN!</a>
         </form>
