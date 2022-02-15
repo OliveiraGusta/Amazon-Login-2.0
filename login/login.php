1e4a6c16bb8a384516fe15b5907cabcc1941d4f0
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-15 00:00:18 -0300

    login and register

--- login/login.php
+++ login/login.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Acessar Amazon</title>
 </head>
 <body>
-    <h1>Login</h1>
+
+    <h1>Fazer login</h1>
+    <form method="POST" action="valid_login.php">
+        <label>E-mail ou número de telefone celular:</label><input type="text" name="" id="" ><br>
+        <label>Senha:</label><input type="password" name="password" id="password"><br>
+        <input type="submit" value="Continuar" id="" name="">
+        </form>
+        <div>
+            Ao continuar, você concorda com as Condições de Uso da Amazon. Por favor verifique a Notificação de Privacidade, Notificação de Cookies e a Notificação de Anúncios Baseados em Interesse.
+       
+        <a>Precisa de ajuda?</a>
+        </div>
+        <div>Novo na Amazon?
+        <a><a href='../'>Criar sua conta da Amazon</a>
+        </div>
 </body>
 </html>
--- register/register.php
+++ register/register.php
@@ -7,6 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>register</h1>
+    <h1>Criar conta</h1>
+
+    <form method="POST" action="register.php">
+        <label>Seu nome:</label><br>
+            <input type="text" name="name" id="name"><br>
+        <label>Número de celular ou e-mail:</label><br>
+            <input type="text" name="" id=""><br>
+        <label>Senha:</label><br>
+            <input type="password" name="" id=""><br>
+            <a>As senhas devem ter pelo menos 6 caracteres.</a><br>
+        <label>Insira a senha nova mais uma vez:</label><br>
+            <input type="password" name="" id=""><br>
+
+        <input type="submit" value="Verificar e-mail" id="register" name="register"><br>
+        </form>
+        <div>
+        <a>
+            Ao criar uma conta, você concorda com as Condições de Uso da Amazon. Por favor verifique a Notificação de Privacidade, Notificação de Cookies e a Notificação de Anúncios Baseados em Interesse.
+</a>
+        </div>
+        <a>Você já tem uma conta?</a><a href='../login/login.php'>Fazer login</a>
 </body>
 </html>
